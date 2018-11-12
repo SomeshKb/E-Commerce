a049d98e619bf501d84e21fe141d8fbe01b0a426
Somesh Bhaskar <somesh.bhaskar@diasparkindia.com>
2018-11-12 19:12:48 +0530

    edited css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ItemsComponent } from './items/items.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardService } from './services/auth-guard.service';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/items', pathMatch: 'full' },
   { path: 'items', component: ItemsComponent },
+  { path: 'item/:id', component: ItemDetailComponent },
   // { path: 'blogs', component: ItemsComponent, canActivate: [AuthGuardService] },
   
 ];
